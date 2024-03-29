87f98d6396f82db86811c26c723c3fabe3771b2e
SuMuCheng <3578557729@qq.com>
2024-03-29 21:15:24 +0800

    Remove CI class

--- build-logic/convention/src/main/kotlin/Versions.kt
+++ build-logic/convention/src/main/kotlin/Versions.kt
@@ -1,14 +1,8 @@
 object Versions {
-    private const val version = "1.0"
-
     const val compileSdkVersion = 34
     const val targetSdkVersion = 34
     const val minSdkVersion = 21
     const val buildToolsVersion = "34.0.0"
     const val versionCode = 1
-    val versionName by lazy {
-        if (CI.isCiBuild) {
-            "$version-${CI.commitHash}-SNAPSHOT"
-        } else version
-    }
+    const val versionName = "1.0.0.0"
 }
