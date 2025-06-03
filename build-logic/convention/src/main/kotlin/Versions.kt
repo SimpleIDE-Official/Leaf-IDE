67d10074525e338ac60835af6ac58afca9330d2e
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-06-03 05:06:15 +0200

    Update Versions.kt

--- build-logic/convention/src/main/kotlin/Versions.kt
+++ build-logic/convention/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
-    const val compileSdkVersion = 34
+    const val compileSdkVersion = 35
     const val targetSdkVersion = 28
     const val minSdkVersion = 21
-    const val buildToolsVersion = "34.0.0"
+    const val buildToolsVersion = "35.0.0"
     const val versionCode = 1
     const val versionName = "1.0.0.0"
 }
